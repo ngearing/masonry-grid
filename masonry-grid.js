32d9e4fee30878659e9518c3a99b8ca058bdbbc7
Nathan Gearing <natgearing@gmail.com>
2024-02-09 13:33:39 +1100

    use intersection observer for grid in accordians

--- masonry-grid.js
+++ masonry-grid.js
@@ -1,50 +1,67 @@
-(function() {
-	
+(function () {
 	const aZ = document.querySelectorAll("#directory");
-	const accord = document.querySelectorAll('.e-n-accordion .e-grid > .e-con-inner');
+	const accord = document.querySelectorAll(
+		".e-n-accordion .e-grid > .e-con-inner"
+	);
 	const grids = [...accord, ...aZ];
-	// const grids = [...aZ];
-	if ( ! grids ) {
+
+	if (!grids) {
 		return;
 	}
 
-	if ( window.innerWidth < 680 ) {
+	if (window.innerWidth < 680) {
 		return;
 	}
-	
+
 	const imgObserver = new MutationObserver((changes) => {
-		changes.forEach(change => {
-		})
+		changes.forEach((change) => {});
 
 		doGrid();
-	})
+	});
+	const gridObserver = new IntersectionObserver(calGrid, {
+		rootMargin: "0px",
+		threshold: 0,
+	});
 
-	
-	
-	window.onload = doGrid();
-	window.addEventListener("resize", doGrid);
+	window.onload = () => {
+		initObservers();
+	};
 
-	function doGrid() {
-		Array.from(grids).forEach(grid => {
-			let rowHeight = parseInt(window.getComputedStyle(grid).getPropertyValue('grid-auto-rows')) || 18;
-			let rowGap = parseInt(window.getComputedStyle(grid).getPropertyValue('grid-row-gap')) || 20;
-			grid.style.gridAutoRows = null;
-			grid.style.gridTemplateRows = 'none';
-
-			const images = grid.querySelectorAll('img');
-			for(let img of images) {
-				imgObserver.observe(img, {attributes: true});
-			}
-
-			Array.from(grid.children).forEach(child => child.style.gridRowEnd = getRowSpan(child,rowHeight,rowGap))
-
-			grid.style.gridAutoRows = rowHeight+'px';
-
-		})
+	function initObservers() {
+		for (let grid of grids) {
+			console.log(grid);
+			gridObserver.observe(grid);
+		}
 	}
 
-	function getRowSpan(item,height,gap) {
-		let rowSpan = Math.ceil((item.getBoundingClientRect().height+gap)/(height+gap));
-		return "span "+rowSpan;
+	function calGrid(entries) {
+		entries.forEach((entry) => {
+			if (entry.isIntersecting) {
+				let grid = entry.target;
+				let rowHeight =
+					parseInt(
+						window.getComputedStyle(grid).getPropertyValue("grid-row-gap")
+					) || 20;
+
+				setGridRows(grid);
+				setGridItemSpan(grid, rowHeight);
+			}
+		});
+	}
+
+	function setGridRows(grid) {
+		grid.style.gridAutoRows = null;
+		grid.style.gridTemplateRows = "none";
+	}
+
+	function setGridItemSpan(grid, rowHeight) {
+		for (let item of grid.children) {
+			item.style.gridRowEnd = setItemSpan(item, rowHeight);
+		}
+	}
+
+	function setItemSpan(item, height) {
+		let rowSpan = Math.ceil(item.getBoundingClientRect().height / height);
+		return "span " + rowSpan;
 	}
 })();
